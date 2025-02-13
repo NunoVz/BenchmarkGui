a1363624d45af59f91214872d733d872ba697127
Vasques <76536544+NunoVz@users.noreply.github.com>
2025-02-13 07:00:37 +0000

    Update app.py

--- gui/backend/app.py
+++ gui/backend/app.py
@@ -9,4 +9,4 @@ def home():
     return render_template("index.html")  # Serve the simple landing page
 
 if __name__ == '__main__':
-    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
+    socketio.run(app, host="0.0.0.0", port=433, debug=True)
